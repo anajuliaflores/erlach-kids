f3d5edcd455ac14f8db1cde60fbc325fece79d29
Ana Julia <anajuliaflores0209@gmail.com>
2026-06-28 12:03:34 -0300

    remove cache service worker

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-// Incrementa a versão para forçar atualização em todos os dispositivos
-const CACHE = 'erlach-kids-v3';
-
-self.addEventListener('install', e => {
-  self.skipWaiting(); // Força ativação imediata
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./index.html','./manifest.json']))
-  );
-});
-
+// Service worker minimalista - sem cache, sempre busca da rede
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim()) // Assume controle imediato
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', e => {
-  // Network first - sempre tenta buscar versão mais recente
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
